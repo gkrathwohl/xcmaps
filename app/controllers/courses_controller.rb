afc9aeba5e6a87a96aab4a410e5165c9ff5cb0fa
Greg Krathwohl <gkrathwohl@gmail.com>
2016-08-13 15:04:05 -0400

    fixed permitted fields since I changed the name of url to map_url

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -69,6 +69,6 @@ class CoursesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def course_params
-      params.require(:course).permit(:name, :url, :description)
+      params.require(:course).permit(:name, :map_url, :description)
     end
 end
